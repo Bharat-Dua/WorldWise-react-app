46dc1aeca1c5c0549b732f1f9c254f771604e583
Bharat Dua <bharatdua13@gmail.com>
2024-08-30 22:15:13 +0530

    feat: adeed  the sidebar component sturcture to render list of cities and countries

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="/price" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="/app/cities" element={<p>cities</p>} />
+          <Route path="/app/countries" element={<p>countries</p>} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
         <Route />
