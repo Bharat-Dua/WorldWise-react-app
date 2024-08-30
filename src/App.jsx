b6b97cf82f6cc07ec928400f55da11c323a6de48
Bharat Dua <bharatdua13@gmail.com>
2024-08-30 14:11:53 +0530

    built the homepage with different nav routes component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
-import PageNav from "./components/PageNav";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
-      <PageNav />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/price" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
         <Route path="/app" element={<AppLayout />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
         <Route />
       </Routes>
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,12 +1,23 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
-        <NavLink to={"/"}>Home </NavLink>
-        <NavLink to={"/price"}>Pricing </NavLink>
-        <NavLink to={"/product"}>Product </NavLink>
+        <li>
+          <NavLink to={"/price"}>Pricing </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/product"}>Product </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/login"} className={styles.ctaLink}>
+            Login
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
